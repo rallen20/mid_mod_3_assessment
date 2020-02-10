08dd5f418caf1dd42dd3018c665105fab7c7d6ea
Ryan Allen <rallenwasd@gmail.com>
2020-02-10 10:48:10 -0700

    Delete pseduo code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,18 +5,4 @@ class SearchController < ApplicationController
       search_results: HouseMembersFacade.new(house)
     }
   end
-
- #  {:_id=>"5a0fa7dcae5bc100213c2338",
- # :name=>"Sirius Black",
- # :house=>"Gryffindor",
- # :school=>"Hogwarts School of Witchcraft and Wizardry",
- # :alias=>"Padfoot",
- # :animagus=>"black dog",
- # :__v=>0,
- # :ministryOfMagic=>false,
- # :orderOfThePhoenix=>true,
- # :dumbledoresArmy=>false,
- # :deathEater=>false,
- # :bloodStatus=>"pure-blood",
- # :species=>"human"}
 end
